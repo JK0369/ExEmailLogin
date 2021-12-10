aa2b0fe8aff5d712e1550b0cb483592c791ab686
jake.k <jake.k@hpcnt.com>
2021-12-10 13:58:21 +0900

    Add test

--- ExEmailLogin/App/SceneDelegate.swift
+++ ExEmailLogin/App/SceneDelegate.swift
@@ -13,7 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
+        // test
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         self.window = window
